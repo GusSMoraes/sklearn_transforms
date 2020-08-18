83b3195ed2230e7ce6e79470fbc8acb045090651
Gustavo Moraes <36543696+GusSMoraes@users.noreply.github.com>
2020-08-18 02:09:48 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -44,7 +44,7 @@ class MediaNA(BaseEstimator, TransformerMixin):
     def transform(self, X):
         data = X.copy()
         
-        media = data.loc[data["PERFIL"] == "EXCELENTE", ["NOTA_GO"]].mean()
+        media = df_data_1.loc[df_data_1["PERFIL"] == "EXCELENTE", ["NOTA_GO"]].mean()
         for index, row in data[(data.PERFIL == "EXCELENTE")].iterrows():
           if(type(row['NOTA_GO']) == float and pd.isna(row['NOTA_GO'])):
             data.loc[data.index == index, 'NOTA_GO'] = media
